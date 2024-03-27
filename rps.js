4990eb60bec13f619d13309064ea6a2956b64b2c
SaraDawner2000 <83845798+SaraDawner2000@users.noreply.github.com>
2024-03-26 22:21:42 -0500

    create playRound function

--- rps.js
+++ rps.js
@@ -3,3 +3,31 @@ function getComputerChoice() {
     computer_selection = Math.floor(Math.random() * 3);
     return game_options[computer_selection];
 }
+function playRound(playerChoice, computerChoice){
+    playerChoice = playerChoice.toLowerCase()
+    if (computerChoice === "rock"){
+        if (playerChoice === "scissors"){
+            return "You Lose! Rock beats Scissors";
+        } else if (playerChoice === "paper"){
+            return "You Win! Paper beats Rock";
+        } else {
+            return "It's a Tie!";
+        }
+    } else if (computerChoice === "paper"){
+        if (playerChoice === "scissors"){
+            return "You Win! Scissors beat Paper";
+        } else if (playerChoice === "rock"){
+            return "You Lose! Paper beats Rock";
+        } else {
+            return "It's a Tie!";
+        }
+    } else {
+        if (playerChoice === "paper"){
+            return "You Lose! Scissors beat Paper";
+        } else if (playerChoice === "rock"){
+            return "You Win! Rock beats Scissors";
+        } else {
+            return "It's a Tie!";
+        }
+    }
+}
