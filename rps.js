a45fa2adf1768aac1422693c780aab454587134f
SaraDawner2000 <83845798+SaraDawner2000@users.noreply.github.com>
2024-03-26 22:39:11 -0500

    create playGame function

--- rps.js
+++ rps.js
@@ -1,33 +1,51 @@
 function getComputerChoice() {
-    game_options = {0:"rock", 1:"paper", 2:"scissors"};
-    computer_selection = Math.floor(Math.random() * 3);
-    return game_options[computer_selection];
+    gameOptions = {0:"rock", 1:"paper", 2:"scissors"};
+    computerSelection = Math.floor(Math.random() * 3);
+    return gameOptions[computerSelection];
 }
 function playRound(playerChoice, computerChoice){
     playerChoice = playerChoice.toLowerCase()
     if (computerChoice === "rock"){
         if (playerChoice === "scissors"){
-            return "You Lose! Rock beats Scissors";
+            return -1;
         } else if (playerChoice === "paper"){
-            return "You Win! Paper beats Rock";
+            return 1;
         } else {
-            return "It's a Tie!";
+            return 0;
         }
     } else if (computerChoice === "paper"){
         if (playerChoice === "scissors"){
-            return "You Win! Scissors beat Paper";
+            return 1;
         } else if (playerChoice === "rock"){
-            return "You Lose! Paper beats Rock";
+            return -1;
         } else {
-            return "It's a Tie!";
+            return 0;
         }
     } else {
         if (playerChoice === "paper"){
-            return "You Lose! Scissors beat Paper";
+            return -1;
         } else if (playerChoice === "rock"){
-            return "You Win! Rock beats Scissors";
+            return 1;
         } else {
-            return "It's a Tie!";
+            return 0;
         }
     }
 }
+
+function playGame(){
+    score = 0;
+    for (let i = 0; i < 5; i++){
+        playerSelection = prompt("Rock, paper or scissors: ").toLowerCase();
+        computerSelection = getComputerChoice();
+        console.log(`You played ${playerSelection} vs ${computerSelection}`);
+        score += playRound(playerSelection, computerSelection);
+    }
+    if (score > 0){
+        return "You won!"
+    } else if (score < 0){
+        return "You lost!"
+    } else {
+        return "It's a tie!"
+    }
+}
+console.log(playGame())
